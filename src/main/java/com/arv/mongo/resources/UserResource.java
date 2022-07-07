b176cc2e5068f038bab658f814944ed2484c66d9
André Ramos <andre_ramosdovale@outlook.com>
2022-07-06 22:42:31 -0300

    Feature: Config mongodb to application

--- src/main/java/com/arv/mongo/resources/UserResource.java
+++ src/main/java/com/arv/mongo/resources/UserResource.java
@@ -1,24 +1,26 @@
 package com.arv.mongo.resources;
 
 import com.arv.mongo.domain.User;
+import com.arv.mongo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        User user1 = new User("1", "maria", "maria@email.com");
-        User user2 = new User("2", "mario", "mario@email.com");
+        List<User> list = service.findAll();
 
-        return ResponseEntity.ok().body(new ArrayList<User>(Arrays.asList(user1, user2)));
+        return ResponseEntity.ok().body(list);
     }
 }
---
+++ src/main/java/com/arv/mongo/service/UserService.java
@@ -0,0 +1,19 @@
+package com.arv.mongo.service;
+
+import com.arv.mongo.domain.User;
+import com.arv.mongo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}
